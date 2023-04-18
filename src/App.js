d3fd20189656030e80a1fd5013bc6d26a1901846
reyhaneh <reyhanehrm333@gmail.com>
2023-04-18 21:39:39 +0430

    use react class component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import Products from "./components/Products/Products";
+import React from "react";
 
-const App = () => {
-  return (
-    <div className="container">
-      <Products title="react" price="99$">
-        This is a good product
-      </Products>
-    </div>
-  );
-};
+class App extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <Products title="react" price="99$">
+          This is a good product
+        </Products>
+      </div>
+    );
+  }
+}
+
+// const App = () => {
+//   return (
+//     <div className="container">
+//       <Products title="react" price="99$">
+//         This is a good product
+//       </Products>
+//     </div>
+//   );
+// };
 
 export default App;
