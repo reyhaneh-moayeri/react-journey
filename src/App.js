27f321095898b90e62910534fee0a659b3f1a231
reyhaneh <reyhanehrm333@gmail.com>
2023-04-18 21:52:09 +0430

    use map to render component for each item in data

--- src/App.js
+++ src/App.js
@@ -2,12 +2,28 @@ import Products from "./components/Products/Products";
 import React from "react";
 
 class App extends React.Component {
+  state = {
+    products: [
+      {
+        title: "react",
+        price: "99$",
+      },
+      {
+        title: "node",
+        price: "109$",
+      },
+    ],
+  };
   render() {
     return (
       <div className="container">
-        <Products title="react" price="99$">
-          This is a good product
-        </Products>
+        {this.state.products.map((product) => {
+          return (
+            <Products title={product.title} price={product.price}>
+              This is a good product
+            </Products>
+          );
+        })}
       </div>
     );
   }
