fc152e27841fc4318f2e9edfbcf619f5fe7dacb5
reyhaneh <reyhanehrm333@gmail.com>
2023-04-18 20:51:20 +0430

    firt render a component

--- src/index.js
+++ src/index.js
@@ -1,17 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// very first simple element handling by react
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import React from "react";
+import ReactDOM from "react-dom";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const heading = React.createElement("h1", {}, "hello there");
+
+const element = React.createElement("div", { id: "section" }, heading);
+
+ReactDOM.render(element, document.querySelector("#root"));
