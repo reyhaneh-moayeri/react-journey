44593010e8383fca27db0e1979677a0802221e53
reyhaneh <reyhanehrm333@gmail.com>
2023-04-23 16:16:57 +0430

    implement class style of updating state based on prev state with callback

--- src/components/HookCounter.js
+++ src/components/HookCounter.js
@@ -1,23 +1,53 @@
-import { useState } from "react";
-const HookCounter = () => {
-  const [counter, setCounter] = useState(0);
+import React, { useState } from "react";
+// const HookCounter = () => {
+//   const [counter, setCounter] = useState(0);
 
-  function addOneHandler() {
-    setCounter(counter + 1);
-  }
+//   function addOneHandler() {
+//     setCounter(counter + 1);
+//   }
 
-  function addFiveHandler() {
+//   function addFiveHandler() {
+//     for (let i = 0; i < 5; i++) {
+//       setCounter((prevState) => prevState + 1);
+//     }
+//   }
+//   return (
+//     <div>
+//       <h1>{counter}</h1>
+//       <button onClick={addOneHandler}>add one</button>
+//       <button onClick={addFiveHandler}>add five</button>
+//     </div>
+//   );
+// };
+
+class HookCounter extends React.Component {
+  state = {
+    counter: 0,
+  };
+
+  addOneHandler = () => {
+    this.setState({ counter: this.state.counter + 1 });
+  };
+
+  addFiveHandler = () => {
     for (let i = 0; i < 5; i++) {
-      setCounter((prevState) => prevState + 1);
+      this.setState((prevState) => {
+        return {
+          counter: prevState.counter + 1,
+        };
+      });
     }
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>{this.state.counter}</h1>
+        <button onClick={this.addOneHandler}>add one</button>
+        <button onClick={this.addFiveHandler}>add five</button>
+      </div>
+    );
   }
-  return (
-    <div>
-      <h1>{counter}</h1>
-      <button onClick={addOneHandler}>add one</button>
-      <button onClick={addFiveHandler}>add five</button>
-    </div>
-  );
-};
+}
 
 export default HookCounter;
