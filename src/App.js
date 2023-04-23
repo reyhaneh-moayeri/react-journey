bff91bd70b5165d2a119e175e40be23192572a82
reyhaneh <reyhanehrm333@gmail.com>
2023-04-23 15:45:52 +0430

    use functional component and useState hook

--- src/App.js
+++ src/App.js
@@ -1,42 +1,104 @@
 import Products from "./components/Products/Products";
-import React from "react";
+import React, { useState } from "react";
 
-class App extends React.Component {
-  state = {
-    products: [
+// class App extends React.Component {
+//   state = {
+//     products: [
+//       {
+//         title: "react",
+//         price: "99$",
+//         description: "this is the good product",
+//       },
+//       {
+//         title: "node",
+//         price: "109$",
+//         description: "learn node js with this course",
+//       },
+//     ],
+//   };
+
+//   clickHandler = () => {
+//     this.setState({
+//       products: [
+// {
+//   title: "vue",
+//   price: "95$",
+//   description: "this is the good product",
+// },
+// {
+//   title: "nest",
+//   price: "89$",
+//   description: "learn node js with this course",
+// },
+//       ],
+//     });
+//   };
+//   render() {
+//     return (
+//   <div className="container">
+//     {this.state.products.map((product) => {
+//       return (
+//         <Products
+//           title={product.title}
+//           price={product.price}
+//           key={product.title}
+//         >
+//           {product.description}
+//         </Products>
+//       );
+//     })}
+
+//     <button onClick={this.clickHandler}>change price</button>
+//   </div>
+//     );
+//   }
+// }
+
+const App = () => {
+  const [products, setProducts] = useState([
+    {
+      title: "vue",
+      price: "95$",
+      description: "this is the good product",
+    },
+    {
+      title: "nest",
+      price: "89$",
+      description: "learn node js with this course",
+    },
+  ]);
+
+  function clickHandler() {
+    setProducts([
       {
         title: "react",
-        price: "99$",
+        price: "95$",
+        description: "this is the good product",
       },
       {
         title: "node",
-        price: "109$",
+        price: "89$",
+        description: "learn node js with this course",
       },
-    ],
-  };
-  render() {
-    return (
-      <div className="container">
-        {this.state.products.map((product) => {
-          return (
-            <Products title={product.title} price={product.price}>
-              This is a good product
-            </Products>
-          );
-        })}
-      </div>
-    );
+    ]);
   }
-}
+  return (
+    <div className="container">
+      {products.map((product) => {
+        return (
+          <Products
+            title={product.title}
+            price={product.price}
+            key={product.title}
+          >
+            {product.description}
+          </Products>
+        );
+      })}
 
-// const App = () => {
-//   return (
-//     <div className="container">
-//       <Products title="react" price="99$">
-//         This is a good product
-//       </Products>
-//     </div>
-//   );
-// };
+      <button onClick={clickHandler}>change price</button>
+    </div>
+  );
+};
 
 export default App;
